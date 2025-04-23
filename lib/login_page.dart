16d1e6079a725093281c0ed23bd38516c6fce5e5
isro24 <alamat_email_anda@example.com>
2025-04-23 08:48:05 +0700

    Menambah TextFormField pada halaman Login

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -10,6 +10,40 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final _formKey = GlobalKey<FormState>();
+    return  Scaffold(
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                controller: emailController,
+                decoration: const InputDecoration(labelText: 'Email'),
+                validator: (value){
+                  if (value == null || value.isEmpty){
+                    return 'Please enter yout email';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: passwordController,
+                decoration: const InputDecoration(labelText: 'Password'),
+                obscureText: true,
+                validator: (value){
+                  if (value == null || value.isEmpty){
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
