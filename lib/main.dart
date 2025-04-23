ca6522e966521e650425d561abed89c608e67380
isro24 <alamat_email_anda@example.com>
2025-04-23 10:05:26 +0700

    Membuat route halaman pada Main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:navigation_routing_parsingparameter/home_page.dart';
 import 'package:navigation_routing_parsingparameter/login_page.dart';
 import 'package:navigation_routing_parsingparameter/order_page.dart';
-import 'package:navigation_routing_parsingparameter/register_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,53 +19,12 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const OrderPage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
+      initialRoute: '/login',
+      routes: {
+        '/login' : (context) => const LoginPage(),
+        '/home' : (context) => const HomePage(),
+        '/order' : (context) => const OrderPage(),
+      },
     );
   }
 }
