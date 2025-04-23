0bf34ae9d45cd78311a1c051e4bc12bb847b044a
isro24 <alamat_email_anda@example.com>
2025-04-23 08:52:29 +0700

    Membuat halaman Register

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:navigation_routing_parsingparameter/login_page.dart';
+import 'package:navigation_routing_parsingparameter/register_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const LoginPage(),
+      home: const RegisterPage(),
     );
   }
 }
